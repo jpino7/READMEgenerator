89485b2816ebf69c13e652e272661849959c7951
JRPino <pino3789@gmail.com>
2020-04-05 15:19:29 -0700

    Created the function for the generateMarkdown and added code as to what it will return

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,45 @@
-function generateMarkdown(data) {
+function generateMarkdown(response, user) {
   return `
-# ${data.title}
+# ${response.project}\n
+
+## Description \n
+${response.description}\n
+
+## Table of Contents \n
+- [Installation](#installation)\n
+- [Usage](#usage)\n
+- [Contributors](#contributors)\n
+- [License](#license)\n
+- [Tests](#tests)\n
+- [Questions](#questions)\n
+
+## Installation \n
+${response.installation}\n
+
+## Usage \n
+${response.usage}\n
+
+## License \n
+${response.license}\n
+
+## Credit \n
+${response.credit}\n
+
+## Contributing \n
+${response.contributing}\n
+
+## Tests \n
+${response.tests}\n
+
+## Questions \n
+
+For any questions please contact the Project Owner:
+
+
+
+
+
+
 
 `;
 }
